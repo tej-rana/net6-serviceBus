47241f935c60354fcc96666703d3d73d8c62033c
Tej Rana <tej@allstrata.tech>
2021-11-13 09:29:15 +1000

    Added new Web project

---
+++ TangoRestaurant/TangoRestaurant.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TangoRestaurant.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
